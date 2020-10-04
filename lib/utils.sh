2c1d9922b6c10f957125969a0526cd8da4bc4bc1
Mancobian Poemandres <mancobian.poemandres@gmail.com>
2020-10-03 21:24:02 -0400

    Add kubectl command.

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 # NOTE: Requires that environment variable are sourced before being called.
 
+function prep_k8s_master() {
+    # Get K8s master
+    K8S_MASTER="$(get_k8s_master_hostname)"
+
+    # Create target directories on Proxmox server
+    ssh ${CI_USER}@${K8S_MASTER}.${SEARCH_DOMAIN} "mkdir -p ${REMOTE_CONFIG_DIR} ${REMOTE_INSTALL_DIR}"
+
+    # Copy k8s files to the remote server
+    rsync -e "ssh -o StrictHostKeyChecking=no" \
+        -aqrvzP ${CONFIG_DIR}/k8s/ \
+        ${CI_USER}@${K8S_MASTER}.${SEARCH_DOMAIN}:${REMOTE_CONFIG_DIR}
+
+    # Copy environment file to the remote server
+    rsync -e "ssh -o StrictHostKeyChecking=no" \
+        -aqrvzP ${INSTALL_DIR}/.env \
+        ${CI_USER}@${K8S_MASTER}.${SEARCH_DOMAIN}:${REMOTE_INSTALL_DIR}
+}
+
 function prep_proxmox_server() {
     # Create target directories on Proxmox server
-    ssh ${PROXMOX_USER}@${PROXMOX_HOST} "mkdir -p ${REMOTE_CONFIG_DIR} ${REMOTE_INSTALL_DIR}"
+    ssh ${PROXMOX_USER}@${PROXMOX_HOST}.${SEARCH_DOMAIN} "mkdir -p ${REMOTE_CONFIG_DIR} ${REMOTE_INSTALL_DIR}"
 
     # Copy config files to the Proxmox server
     rsync --exclude .git \
-        -arvzP ${CONFIG_DIR}/ \
-        ${PROXMOX_USER}@${PROXMOX_HOST}:${REMOTE_CONFIG_DIR}
+        -aqrvzP ${CONFIG_DIR}/ \
+        ${PROXMOX_USER}@${PROXMOX_HOST}.${SEARCH_DOMAIN}:${REMOTE_CONFIG_DIR}
 
     # Copy homelab files to the Proxmox server
-    rsync -arvzP --rsh=ssh ${INSTALL_DIR}/ \
+    rsync -aqrvzP ${INSTALL_DIR}/ \
         ${CI_SSH_KEY_PATH}/${CI_PUBLIC_KEY_FILE} \
         ${CI_SSH_KEY_PATH}/${CI_PRIVATE_KEY_FILE} \
-        ${PROXMOX_USER}@${PROXMOX_HOST}:${REMOTE_INSTALL_DIR}
+        ${PROXMOX_USER}@${PROXMOX_HOST}.${SEARCH_DOMAIN}:${REMOTE_INSTALL_DIR}
 }
 
 function get_k8s_master_hostname() {
